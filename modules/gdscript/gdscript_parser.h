c81f9b4f06232e50d1af7ce68dcda00e948406fa
Paucey <43685777+Paucey@users.noreply.github.com>
2024-10-31 15:52:58 -0300

    Another add_member tweak, again.

--- modules/gdscript/gdscript_parser.h
+++ modules/gdscript/gdscript_parser.h
@@ -799,7 +799,7 @@ public:
     			String name = p_member_node->identifier->name;
 
     			// Check if the function name already exists in `members_indices`
-    			if (members_indices.has(name) && (p_member_node->get_source_node() == function)) {
+    			if (members_indices.has(name) && (p_member_node.get_type_name() == "function")) {
         			// Retrieve the existing function's index
         			int existing_index = members_indices[name];
 

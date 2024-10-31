f562ce85006f6dc531919168ac1e2ee4afaafefe
Paucey <43685777+Paucey@users.noreply.github.com>
2024-10-31 20:55:14 -0300

    Update gdscript_parser.cpp

--- modules/gdscript/gdscript_parser.cpp
+++ modules/gdscript/gdscript_parser.cpp
@@ -974,7 +974,7 @@ void GDScriptParser::parse_class_member(T *(GDScriptParser::*p_parse_function)(b
 					push_error(vformat(R"(%s "%s" has the same name as a previously declared %s.)", p_member_kind.capitalize(), member->identifier->name, current_class->get_member(member->identifier->name).get_type_name()), member->identifier);
 				}
 			} else {
-				current_class->add_member(member);
+				current_class->add_duplicate_member(member);
 			}
 		} else {
 			current_class->add_member(member);

b051ddd38fab5da5e0d5280e63a33ae8761212b1
Paucey <43685777+Paucey@users.noreply.github.com>
2024-10-31 17:51:20 -0300

    Update gdscript_parser.h

--- modules/gdscript/gdscript_parser.h
+++ modules/gdscript/gdscript_parser.h
@@ -798,7 +798,7 @@ public:
             				int existing_index = members_indices[name];
 
             				// Access the existing function in the members list
-            				Member &existing_member = const_cast<Member&>(members[existing_index]);
+            				Member &existing_member = Member&(members[existing_index]);
 
             				// Append logic to the existing function's body
             				existing_member.append_logic(func_node->body);
